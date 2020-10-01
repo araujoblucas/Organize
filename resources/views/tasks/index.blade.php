2fffebfe6a4248e1f12f7a5cfcf13046325de017
Lucas Araujo <lucas.araujo@atlastechnol.com>
2020-10-01 18:28:21 -0300

    fix layout

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,6 +15,30 @@
 </head>
 <body>
 <div class="flex flex-col items-center justify-center">
+    <div class="flex w-8/12 h-5 mt-3 rounded border-4 items-center text-center flex-col mb-5" style="height: 35vh; border-color: #4D4668; font-family: 'Roboto', sans-serif;">
+        <div class="text-3xl mt-5 font-bold" style="color: #4D4668">Cadastro de Tasks Semanais</div>
+        <form method="post" action="{{route('task.weeklyStore')}}">
+            @method('post') @csrf
+            <div class="flex w-12/12 mt-5 justify-center align-center items-center">
+                <label class="p-3" >Descrição:</label>
+                <input class="p-3 bg-gray-100 border-black rounded border-r-2 border-b-2 w-8/12" name="desc" />
+            </div>
+
+
+            <div class="flex flex-row w-12/12 ">
+                <div>
+                    <label class="mr-5">Dia que começa:</label>
+                    <input class="bg-gray-100 mt-3 rounded" type="datetime-local" name="finalWeekDate" />
+                </div>
+                <div class="ml-6">
+                    <label class="mr-5">Dia que termina:</label>
+                    <input class="bg-gray-100 mt-3 rounded" type="datetime-local" name="created_at" />
+                </div>
+            </div>
+
+            <button class="mt-5 p-3 rounded px-8 bg-gray-100" type="submit">Enviar</button>
+        </form>
+    </div>
     <div class="flex w-11/12 h-5 mt-3 rounded border-4 text-center flex-col " style="border-color: #4D4668; height: 70vh; font-family: 'Roboto', sans-serif; ">
         <div class="w-full flex flex-col">
             <div class="flex py-4 border-b-4 border-gray-300 w-full">
@@ -54,7 +78,7 @@
                     </div>
                     <div class="flex w-2/12 text-md justify-center ">
                         <div class="dropdown inline-block relative">
-                            <a  onclick="myFunction({{$key}})" href="#"><i class="fa fa-bars" aria-hidden="true"></i></a>
+                            <a  onclick="myFunction({{$key}})" href="#"><i class="fa fa-trash" aria-hidden="true"></i></a>
                             <ul id="dropdown-menu{{$key}}" class="dropdown-menu absolute text-gray-700 pt-1" style="display: none">
                                 <li class=""><a class="rounded-t bg-gray-100 hover:bg-gray-200 py-2 px-4 block whitespace-no-wrap" href="#">Editar</a></li>
                                 <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Deletar</a></li>
@@ -65,30 +89,6 @@
                 </div>
             @endforeach
         </div>
-    </div>
-    <div class="flex w-8/12 h-5 mt-3 rounded border-4 items-center text-center flex-col mb-5" style="height: 35vh; border-color: #4D4668; font-family: 'Roboto', sans-serif;">
-        <div class="text-3xl mt-5 font-bold" style="color: #4D4668">Cadastro de Tasks Semanais</div>
-        <form method="post" action="{{route('task.weeklyStore')}}">
-            @method('post') @csrf
-            <div class="flex w-12/12 mt-5 justify-center align-center items-center">
-                <label class="p-3" >Descrição:</label>
-                <input class="p-3 bg-gray-100 border-black rounded border-r-2 border-b-2 w-8/12" name="desc" />
-            </div>
-
-
-            <div class="flex flex-row w-12/12 ">
-                <div>
-                    <label class="mr-5">Dia que começa:</label>
-                    <input class="bg-gray-100 mt-3 rounded" type="date" name="finalWeekDate" />
-                </div>
-                <div class="ml-6">
-                    <label class="mr-5">Dia que termina:</label>
-                    <input class="bg-gray-100 mt-3 rounded" type="date" name="created_at" />
-                </div>
-            </div>
-
-            <button class="mt-5 p-3 rounded px-8 bg-gray-100" type="submit">Enviar</button>
-        </form>
     </div>
 </div>
 
